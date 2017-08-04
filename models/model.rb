651a8a88bb1736744858b87efebfa4f630e9be60
Sydney James <jammar9903@comcast.net>
2017-08-04 16:57:09 +0000

    final_travel360

--- models/model.rb
+++ models/model.rb
@@ -6,30 +6,37 @@ def type_of_trip(type, distance, season, temperature)
             if season == "Summer"
                 if temperature == "Warm"
                     answer = "Santorini, Greece"
-                    video_link = "youtube.com/video/12345"
+                    video_link = "https://www.youtube.com/embed/cMcSU7NTAPc"
                 elsif temperature == "Cold"
                     answer = "Bruges, Belguim"
+                    video_link = "https://www.youtube.com/embed/LvYkxXEgQj8"
                 end
                 
             elsif season == "Fall"
                 if temperature == "Warm"
                     answer = "Venice, Italy"
+                    video_link = "https://www.youtube.com/embed/ka-ZgwCXKho"
                 elsif temperature == "Cold"
                     answer = "Paris, France"
+                    video_link = "https://www.youtube.com/embed/AQ6GmpMu5L8"
                 end
                 
             elsif season == "Winter"
                 if temperature == "Warm"
                     answer = "Ko Ngai, Thailand"
+                    video_link = "https://www.youtube.com/embed/p4oMhFKsSoA"
                 elsif temperature == "Cold"
-                    answer = "Iceland"
+                    answer = "Reykjavik, Iceland"
+                    video_link = "https://www.youtube.com/embed/p4oMhFKsSoA"
                 end
                 
             elsif season == "Spring"
                 if temperature == "Warm"
-                    answer = "Bora Bora"    
+                    answer = "Bora Bora"   
+                    video_link = "https://www.youtube.com/embed/wRzoewOkafk"
                 elsif temperature == "Cold"
                     answer = "Oberoi Udaivilas, Udaipur, India"
+                    video_link = "https://www.youtube.com/embed/UoIjjPqqnO0"
                 end
             end
 
@@ -39,29 +46,37 @@ def type_of_trip(type, distance, season, temperature)
             if season == "Summer"
                 if temperature == "Warm"
                     answer = "St. Paul, Minnesota"
+                    video_link = "https://www.youtube.com/embed/I8oBC6jo7i8"
                 elsif temperature == "Cold"
                     answer = "Vancouver, Canada"
+                    video_link = "https://www.youtube.com/embed/hU6a-b6ADSE"
                 end
                     
             elsif season == "Fall"
                 if temperature == "Warm"
                     answer = "Napa Valley, California"
+                    video_link = "https://www.youtube.com/embed/W_E5wEMbYxw"
                 elsif temperature == "Cold"
                     answer = "Tofino BC, Canada"
+                    video_link = "https://www.youtube.com/embed/KGnoGpi7Dmo"
                 end
                     
             elsif season == "Winter"
                 if temperature == "Warm"
                     answer = "Los Cabos, Mexico"
+                    video_link = "https://www.youtube.com/embed/J97dBK6Jq3U"
                 elsif temperature == "Cold"
                     answer = "Vail, Colorado" 
+                    video_link = "https://www.youtube.com/embed/_JYYhkopnrA"
                 end
                     
             elsif season == "Spring"
                 if temperature == "Warm"
                     answer = "Monterrey Peninsula, California"
+                    video_link = "https://www.youtube.com/embed/EJ9jqF4scsk"
                 elsif temperature == "Cold"
                     answer = "Niagra Falls"
+                    video_link = "https://www.youtube.com/embed/IVKXywZ6hPk"
                 end
             end
         end
@@ -75,27 +90,34 @@ def type_of_trip(type, distance, season, temperature)
             if season == "Summer"
                 if temperature == "Warm"
                     answer = "Ocean City, Maryland "
+                    video_link = "https://www.youtube.com/embed/JqAnO7vqefY"
                 elsif temperature == "Cold"
                     answer = "Vancouver, Canada"
+                    video_link = "https://www.youtube.com/embed/hU6a-b6ADSE"
                 end
                         
             elsif season == "Fall"
                 if temperature == "Warm"
                     answer = "Hilton Head Island, South Carolina"
+                    video_link = "https://www.youtube.com/embed/L6oF-sG7Bdc"
                 elsif temperature == "Cold"
                     answer = "Branson, Missouri"
+                    video_link = "https://www.youtube.com/embed/LgDMYobhgcU"
                 end
                     
             elsif season == "Winter"
                 if temperature == "Warm"
                     answer = "Pheonix, Arizona"
+                    video_link = "https://www.youtube.com/embed/j9iP5hezIgw"
                 elsif temperature == "Cold"
                     answer = "Orlando, Florida"
+                    video_link = "https://www.youtube.com/embed/LfCsOwMc3hk"
                 end
                     
             elsif season == "Spring"
                 if temperature == "Warm"
                     answer = "Grand Canyon(Arizona)"
+                    video_link = "https://www.youtube.com/embed/Eh_5Pnv39cY"
                 elsif temperature == "Cold"
                     answer = "San Francisco, California"
                 end
@@ -108,6 +130,7 @@ def type_of_trip(type, distance, season, temperature)
                 
                 if temperature == "Warm"
                     answer = "Madrid, Spain"
+                    video_link= "https://www.youtube.com/embed/hREcgljzKPA"
                 elsif temperature == "Cold"
                     answer = "Scotland"
                 end
@@ -221,11 +244,13 @@ def type_of_trip(type, distance, season, temperature)
                     answer = "Yogyakarta, Indonesia"
                 elsif temperature == "Cold"
                     answer = "Island of Bute, Scotland"
+                    video_link= https://www.youtube.com/embed/C2o4suMs3I
                 end
                 
             elsif season == "Winter"
                 if temperature == "Warm"
                     answer= "Rio de Janeiro, Brazil"
+                    video_link= https://www.youtube.com/embed/ieWNzZPfZzk
                 elsif temperature == "Cold"
                     answer = "Blue Lagoon, Iceland"
                 end
@@ -235,6 +260,7 @@ def type_of_trip(type, distance, season, temperature)
                     answer = "Galapagos Islands, Ecuador"
                 elsif temperature == "Cold"
                     answer = "Island of Bute, Scotland"
+                    video_link= https://www.youtube.com/embed/C2o4suMs3I
                 end
             end
     
@@ -275,7 +301,7 @@ def type_of_trip(type, distance, season, temperature)
            
             
     
-return answer
+return answer, video_link
 end
 
 
